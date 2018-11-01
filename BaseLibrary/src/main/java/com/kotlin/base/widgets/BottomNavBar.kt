b475c31803d4438416db0301b9259833f346cee5
zhuhongfu <zhuhongfu@hyjf.com>
2018-11-01 16:53:03 +0800

    添加底部导航栏

---
+++ BaseLibrary/src/main/java/com/kotlin/base/widgets/BottomNavBar.kt
@@ -0,0 +1,109 @@
+package com.kotlin.base.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import com.ashokvarma.bottomnavigation.BottomNavigationBar
+import com.ashokvarma.bottomnavigation.BottomNavigationItem
+import com.ashokvarma.bottomnavigation.ShapeBadgeItem
+import com.ashokvarma.bottomnavigation.TextBadgeItem
+import com.kotlin.base.R
+
+
+/**
+ *
+ *  Desc: 底部导航
+ *
+ * @author zhf QQ:578121695
+ * @time 2018/11/1 15:34
+ */
+class BottomNavBar @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : BottomNavigationBar(context, attrs, defStyleAttr) {
+
+    //购物车Tab 标签
+    private val mCartBadge: TextBadgeItem
+    //消息Tab 标签
+    private val mMsgBadge: ShapeBadgeItem
+
+    init {
+        //首页
+        val homeItem = BottomNavigationItem(R.drawable.btn_nav_home_press, resources.getString(R.string.nav_bar_home))  // 文案 和 选中状态下图片
+                .setInactiveIconResource(R.drawable.btn_nav_home_normal) // 未选中状态下图标
+                .setActiveColorResource(R.color.common_blue) // 选择时 字体颜色
+                .setInActiveColorResource(R.color.text_normal) // 未选择时字体颜色
+
+        //分类
+        val categoryItem = BottomNavigationItem(R.drawable.btn_nav_category_press, resources.getString(R.string.nav_bar_category))
+                .setInactiveIconResource(R.drawable.btn_nav_category_normal)
+                .setActiveColorResource(R.color.common_blue)
+                .setInActiveColorResource(R.color.text_normal)
+
+        //购物车
+        val cartItem = BottomNavigationItem(R.drawable.btn_nav_cart_press, resources.getString(R.string.nav_bar_cart))
+                .setInactiveIconResource(R.drawable.btn_nav_cart_normal)
+                .setActiveColorResource(R.color.common_blue)
+                .setInActiveColorResource(R.color.text_normal)
+
+        // 添加购物车右上角的数字样式
+        mCartBadge = TextBadgeItem()
+        cartItem.setBadgeItem(mCartBadge)
+        // 设置显示的购物车的数量
+//        mCartBadge.setText("10")
+
+        //消息
+        val msgItem = BottomNavigationItem(R.drawable.btn_nav_msg_press, resources.getString(R.string.nav_bar_msg))
+                .setInactiveIconResource(R.drawable.btn_nav_msg_normal)
+                .setActiveColorResource(R.color.common_blue)
+                .setInActiveColorResource(R.color.text_normal)
+
+        // 添加消息右上角的数字样式
+        mMsgBadge = ShapeBadgeItem()
+        mMsgBadge.setShape(ShapeBadgeItem.SHAPE_OVAL)
+        msgItem.setBadgeItem(mMsgBadge)
+
+        //我的
+        val userItem = BottomNavigationItem(R.drawable.btn_nav_user_press, resources.getString(R.string.nav_bar_user))
+                .setInactiveIconResource(R.drawable.btn_nav_user_normal)
+                .setActiveColorResource(R.color.common_blue)
+                .setInActiveColorResource(R.color.text_normal)
+
+        //设置底部导航模式及样式
+        setMode(BottomNavigationBar.MODE_FIXED)
+        // 设置背景模式
+        setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC)
+        // 设置背景颜色
+        setBarBackgroundColor(R.color.common_white)
+        //添加Tab
+        addItem(homeItem)
+                .addItem(categoryItem)
+                .addItem(cartItem)
+                .addItem(msgItem)
+                .addItem(userItem)
+                .setFirstSelectedPosition(0) // 默认选中第一个
+                .initialise()
+    }
+
+    /*
+        检查购物车Tab是否显示标签
+     */
+    fun checkCartBadge(count: Int) {
+        if (count == 0) {
+            mCartBadge.hide()
+        } else {
+            mCartBadge.show()
+            // 设置显示的购物车的数量
+            mCartBadge.setText("$count")
+        }
+    }
+
+    /*
+        检查消息Tab是否显示标签
+     */
+    fun checkMsgBadge(isVisiable: Boolean) {
+        if (isVisiable) {
+            mMsgBadge.show()
+        } else {
+            mMsgBadge.hide()
+        }
+    }
+}
--- app/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
+++ app/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -3,12 +3,31 @@ package com.kotlin.mall.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.mall.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-
+// 主界面
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBottomNavBar.checkCartBadge(10)
+        mBottomNavBar.checkMsgBadge(false)
+
+        Observable.timer(2,TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({mBottomNavBar.checkMsgBadge(true)})
+
+         Observable.timer(5,TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({mBottomNavBar.checkCartBadge(0)})
+
+
+
     }
 }
