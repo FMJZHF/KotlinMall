fcf15741a4d28753a87afd2cc6fd526c1a36bac6
zhuhongfu <zhuhongfu@hyjf.com>
2018-11-09 20:22:16 +0800

    订单管理

--- OrderCenter/src/main/java/com/kotlin/order/injection/component/OrderComponent.kt
+++ OrderCenter/src/main/java/com/kotlin/order/injection/component/OrderComponent.kt
@@ -5,7 +5,7 @@ import com.kotlin.base.injection.component.ActivityComponent
 import com.kotlin.order.injection.module.OrderModule
 import com.kotlin.order.ui.activity.OrderConfirmActivity
 //import com.kotlin.order.ui.activity.OrderDetailActivity
-//import com.kotlin.order.ui.fragment.OrderFragment
+import com.kotlin.order.ui.fragment.OrderFragment
 import dagger.Component
 
 /*
@@ -15,7 +15,7 @@ import dagger.Component
 @Component(dependencies = arrayOf(ActivityComponent::class), modules = arrayOf(OrderModule::class))
 interface OrderComponent {
     fun inject(activity: OrderConfirmActivity)
-//    fun inject(fragment: OrderFragment)
-//
+    fun inject(fragment: OrderFragment)
+
 //    fun inject(activity: OrderDetailActivity)
 }
--- app/src/main/java/com/kotlin/mall/ui/fragment/MeFragment.kt
+++ app/src/main/java/com/kotlin/mall/ui/fragment/MeFragment.kt
@@ -11,10 +11,10 @@ import com.kotlin.base.ui.fragment.BaseFragment
 import com.kotlin.base.utils.AppPrefsUtils
 import com.kotlin.mall.R
 import com.kotlin.mall.ui.activity.SettingActivity
-//import com.kotlin.order.common.OrderConstant
-//import com.kotlin.order.common.OrderStatus
-//import com.kotlin.order.ui.activity.OrderActivity
-//import com.kotlin.order.ui.activity.ShipAddressActivity
+import com.kotlin.order.common.OrderConstant
+import com.kotlin.order.common.OrderStatus
+import com.kotlin.order.ui.activity.OrderActivity
+import com.kotlin.order.ui.activity.ShipAddressActivity
 import com.kotlin.provider.common.ProviderConstant
 import com.kotlin.provider.common.afterLogin
 import com.kotlin.provider.common.isLogined
@@ -93,39 +93,34 @@ class MeFragment : BaseFragment(), View.OnClickListener {
     override fun onClick(view: View) {
         when (view.id) {
             R.id.mUserIconIv, R.id.mUserNameTv -> {
-//                afterLogin {
-//                    startActivity<UserInfoActivity>()
-//                }
-                if(isLogined()){
+                afterLogin {
                     startActivity<UserInfoActivity>()
-                }else{
-                    startActivity<LoginActivity>()
                 }
             }
-//
-//            R.id.mWaitPayOrderTv -> {
-//                startActivity<OrderActivity>(OrderConstant.KEY_ORDER_STATUS to OrderStatus.ORDER_WAIT_PAY)
-//            }
-//            R.id.mWaitConfirmOrderTv -> {
-//                startActivity<OrderActivity>(OrderConstant.KEY_ORDER_STATUS to OrderStatus.ORDER_WAIT_CONFIRM)
-//            }
-//            R.id.mCompleteOrderTv -> {
-//                startActivity<OrderActivity>(OrderConstant.KEY_ORDER_STATUS to OrderStatus.ORDER_COMPLETED)
-//            }
-//            R.id.mAllOrderTv -> {
-//                afterLogin {
-//                    startActivity<OrderActivity>()
-//                }
-//            }
-//
-//            R.id.mAddressTv -> {
-//                afterLogin {
-//                    startActivity<ShipAddressActivity>()
-//                }
-//            }
-//            R.id.mShareTv -> {
-//                toast(R.string.coming_soon_tip)
-//            }
+
+            R.id.mWaitPayOrderTv -> {
+                startActivity<OrderActivity>(OrderConstant.KEY_ORDER_STATUS to OrderStatus.ORDER_WAIT_PAY)
+            }
+            R.id.mWaitConfirmOrderTv -> {
+                startActivity<OrderActivity>(OrderConstant.KEY_ORDER_STATUS to OrderStatus.ORDER_WAIT_CONFIRM)
+            }
+            R.id.mCompleteOrderTv -> {
+                startActivity<OrderActivity>(OrderConstant.KEY_ORDER_STATUS to OrderStatus.ORDER_COMPLETED)
+            }
+            R.id.mAllOrderTv -> {
+                afterLogin {
+                    startActivity<OrderActivity>()
+                }
+            }
+
+            R.id.mAddressTv -> {
+                afterLogin {
+                    startActivity<ShipAddressActivity>()
+                }
+            }
+            R.id.mShareTv -> {
+                toast(R.string.coming_soon_tip)
+            }
             R.id.mSettingTv -> {
                 startActivity<SettingActivity>()
             }
